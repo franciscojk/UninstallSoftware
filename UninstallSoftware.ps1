373f368397a7d495987c1c05e54ef5f70c243737
franciscojk <58817943+franciscojk@users.noreply.github.com>
2019-12-30 14:53:59 -0300

    Update UninstallSoftware.ps1

--- UninstallSoftware.ps1
+++ UninstallSoftware.ps1
@@ -1,19 +1,21 @@
 param(
 	[parameter(Mandatory=$false)]
 	[ValidateNotNullOrEmpty()]
-	[String]$Software = "ADOTI")
+	[String]$Software = "Remote Desktop")
 
-$ADOTIInstalado = Get-WmiObject -Class Win32_Product | Where-Object {$_.name -match $Software}
+$SoftwareInstalado = Get-WmiObject -Class Win32_Product | Where-Object {$_.name -match $Software}
 
-if ($ADOTIInstalado -eq $null){
+if ($SoftwareInstalado -eq $null){
     Write-Host $Software + " nao esta instalado!"
 }else{
     if ((Get-WmiObject Win32_OperatingSystem).OSArchitecture -match "64"){
-        $ADOTIReg = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\" + $ADOTIInstalado.IdentifyingNumber
+        $ADOTIReg = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\" + $SoftwareInstalado.IdentifyingNumber
     }else{
-        $ADOTIReg = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\" + $ADOTIInstalado.IdentifyingNumber
+        $ADOTIReg = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\" + $SoftwareInstalado.IdentifyingNumber
     }
-    $command = (Get-Item -Path $ADOTIReg | Get-ItemProperty).UninstallString + " INSTALL_PASSWORD=6B6F637967667E6266212431"
-    icm -command $command
-    Wait-Process -Name msiexec
+    $command = ((Get-Item -Path $ADOTIReg | Get-ItemProperty).UninstallString + " INSTALL_PASSWORD=6B6F637967667E6266212431 /quiet").Replace("{","""{").Replace("}","}""")
+    $setup = $command.Substring(0,11)
+    $args = $command.Substring(12,$command.Length -12)
+
+    Start-Process $setup -Wait -ArgumentList $args
 }
